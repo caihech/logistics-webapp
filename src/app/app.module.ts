890a4d70b0598b2166c92461139d9d1d49568018
Cai He <h.cai@s2icode.com>
2018-07-04 16:34:11 +0800

    add    @angular/flex-layout"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
-import { SharedModule } from './shared/shared.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app.routing.module';
+import {SharedModule} from './shared/shared.module';
 
-
-
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ConsignmentNoteComponent } from './consignment-note/consignment-note.component';
-import { Code404Component } from './code404/code404.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {UsersComponent} from './users/users.component';
+import {ConsignmentNoteComponent} from './consignment-note/consignment-note.component';
+import {Code404Component} from './code404/code404.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    UsersComponent,
-    ConsignmentNoteComponent,
-    Code404Component
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    AppRoutingModule
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent,
+        UsersComponent,
+        ConsignmentNoteComponent,
+        Code404Component
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        BrowserAnimationsModule,
+        SharedModule,
+        AppRoutingModule
 
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
