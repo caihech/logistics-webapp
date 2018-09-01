ee2fe8407cf1626e976f459359b2f1fcc40f5cf8
Cai He <h.cai@s2icode.com>
2018-09-01 15:31:58 +0800

    添加相关组件路由

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -10,6 +10,19 @@ import {AdminComponent} from './admin/admin.component';
 import {HomeComponent} from './admin/home/home.component';
 
 
+import {UsersComponent} from './admin/users/users.component';
+import {UserAddComponent} from './admin/users/user-add/user-add.component';
+import {UserDetailComponent} from './admin/users/user-detail/user-detail.component';
+
+import {ConsignmentNotesComponent} from './admin/consignment-notes/consignment-notes.component';
+import {ConsignmentNoteAddComponent} from './admin/consignment-notes/consignment-note-add/consignment-note-add.component';
+import {ConsignmentNoteDetailComponent} from './admin/consignment-notes/consignment-note-detail/consignment-note-detail.component';
+
+import {VehiclesComponent} from './admin/vehicles/vehicles.component';
+import {VehicleAddComponent} from './admin/vehicles/vehicle-add/vehicle-add.component';
+import {VehicleDetailComponent} from './admin/vehicles/vehicle-detail/vehicle-detail.component';
+
+
 const routes: Routes = [
 
     {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -19,6 +32,18 @@ const routes: Routes = [
         path: 'admin',
         component: AdminComponent, children: [
         {path: '', component: HomeComponent},
+
+        {path: 'users', component: UsersComponent},
+        {path: 'user/add', component: UserAddComponent},
+        {path: 'user/detail', component: UserDetailComponent},
+
+        {path: 'consignmentnotes', component: ConsignmentNotesComponent},
+        {path: 'consignmentnote/add', component: ConsignmentNoteAddComponent},
+        {path: 'consignmentnote/detail', component: ConsignmentNoteDetailComponent},
+
+        {path: 'vehicles', component: VehiclesComponent},
+        {path: 'vehicle/add', component: VehicleAddComponent},
+        {path: 'vehicle/detail', component: VehicleDetailComponent}
     ]
     },
     {path: 'error-page', component: ErrorPageComponent},
