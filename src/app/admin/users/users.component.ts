21f99c30ac4e6a3530181c15726b3800c4a71287
Cai He <h.cai@s2icode.com>
2018-10-10 17:20:29 +0800

    实现用户集合

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UsersService} from './users.service';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+    usersTableDisplayedColumns: string[] = ['id', 'username', 'fullname', 'mobilephone', 'roleName', 'stateName', 'updateTime', 'edit', 'delete'];
+    usersTableDataSource;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngOnInit() {
-  }
 
+    constructor(private usersService: UsersService) {
+    }
+
+    ngOnInit() {
+        this.httpGetUsers();
+    }
+
+
+    /**
+     * 获取用户列表
+     */
+    httpGetUsers() {
+        let _that = this;
+        this.usersService.getUsers().subscribe(
+            function (rep) {
+                _that.usersTableDataSource = new MatTableDataSource<Object[]>(rep);
+                _that.usersTableDataSource.paginator = _that.paginator;
+            }, function (error) {
+                console.error(error);
+            }
+        );
+    }
+
+    onEdit(id: number) {
+        alert('编辑用户' + id);
+    }
+
+    onDelete(id: number) {
+        alert('删除用户' + id);
+    }
 }
--- src/app/admin/users/users.service.ts
+++ src/app/admin/users/users.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/index';
+import {SharedService} from '../../shared/shared.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+
+
+    /**
+     * 获取用户信息集合 需要登录
+     * @returns {Observable<Object>}
+     */
+    getUsers(): Observable<Object> {
+        return this.http.get(SharedService.getUrl + '/users', SharedService.getLoginHttpHeader);
+    }
 }
