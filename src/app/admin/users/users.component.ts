56801596ba94a0b029d828795774745e005bf179
Cai He <h.cai@s2icode.com>
2018-10-10 19:51:08 +0800

    添加排序

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {UsersService} from './users.service';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
     selector: 'app-users',
@@ -9,9 +9,11 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';
 })
 export class UsersComponent implements OnInit {
 
-    usersTableDisplayedColumns: string[] = ['id', 'username', 'fullname', 'mobilephone', 'roleName', 'stateName', 'updateTime', 'edit', 'delete'];
+    usersTableDisplayedColumns: string[] = ['id', 'username', 'fullname', 'mobilephone', 'roleName',
+        'stateName', 'updateTime', 'edit', 'delete'];
     usersTableDataSource;
     @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
 
 
     constructor(private usersService: UsersService) {
@@ -29,18 +31,27 @@ export class UsersComponent implements OnInit {
         let _that = this;
         this.usersService.getUsers().subscribe(
             function (rep) {
-                _that.usersTableDataSource = new MatTableDataSource<Object[]>(rep);
+                _that.usersTableDataSource = new MatTableDataSource<Object>(rep);
                 _that.usersTableDataSource.paginator = _that.paginator;
+                _that.usersTableDataSource.sort = _that.sort;
             }, function (error) {
                 console.error(error);
             }
         );
     }
 
+    /**
+     * 修改用户信息通过用户id
+     * @param {number} id
+     */
     onEdit(id: number) {
         alert('编辑用户' + id);
     }
 
+    /**
+     * 删除用户
+     * @param {number} id
+     */
     onDelete(id: number) {
         alert('删除用户' + id);
     }
