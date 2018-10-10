892c0f685bf1468e769fd1be6cf26f5c7300e0a4
Cai He <h.cai@s2icode.com>
2018-10-10 21:37:35 +0800

    修改头部导航

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {SharedService} from '../shared/shared.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-admin',
@@ -8,14 +10,46 @@ import {Component, OnInit} from '@angular/core';
 export class AdminComponent implements OnInit {
 
     openedNavBar = true;
+    username: String = '';
 
-    constructor() {
+    constructor(private router: Router) {
     }
 
     ngOnInit() {
+        this.permission();
     }
 
 
+    /**
+     * 权限验证
+     */
+    permission() {
+        var that = this;
+        this.username = SharedService.getStorageItem('username');
+        if (!this.username) {
+            alert('请登录');
+            that.onExitLogin();
+            return;
+        } else {
+            // todo 验证tokenExpired与当前时间对比,并且判断非空
+            console.info(SharedService.getStorageItem('tokenExpired'))
+            console.info('成功');
+        }
+    }
+
+    /**
+     * 退出登录
+     */
+    onExitLogin() {
+        SharedService.removeStorageItem('username');
+        SharedService.removeStorageItem('token');
+        this.router.navigate(['/login']);
+    }
+
+    /**
+     * 左侧导航开关
+     * @param e
+     */
     onOpendNavBar(e) {
         if (this.openedNavBar) {
             this.openedNavBar = false;
