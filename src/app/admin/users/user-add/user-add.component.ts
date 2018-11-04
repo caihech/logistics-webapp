875617103cb206b9d72c6f4c705f3dd39016f529
Cai He <h.cai@s2icode.com>
2018-11-04 23:02:55 +0800

    添加用户form表单ui

--- src/app/admin/users/user-add/user-add.component.ts
+++ src/app/admin/users/user-add/user-add.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-user-add',
-  templateUrl: './user-add.component.html',
-  styleUrls: ['./user-add.component.css']
+    selector: 'app-user-add',
+    templateUrl: './user-add.component.html',
+    styleUrls: ['./user-add.component.css']
 })
 export class UserAddComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    userFormModel: FormGroup;
 
+
+    constructor(fb: FormBuilder) {
+
+        this.userFormModel = fb.group({
+                username: ['', [Validators.required]],
+                password: ['', [Validators.required]],
+                mobilephone: ['', [Validators.required]],
+                email: ['', [Validators.required]],
+                fullname: [''],
+                birthday: [''],
+                companyName: [''],
+                postalCode: [''],
+                address: [''],
+                fax: [''],
+                telephone: [''],
+                wechat: [''],
+                weibo: [''],
+                remark: ['']
+
+            }
+        );
+
+    }
+
+    ngOnInit() {
+    }
+
+
+    /**
+     * 提交表单
+     */
+    onSubmitForm() {
+        console.info(this.userFormModel.value);
+    }
 }
+
+
+// userRet.setSex(user.getSex());
+// userRet.setValid(true);
