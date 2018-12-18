693e695c1fd1c0fe0dc991350af92add973c02c5
Dawei Shao <dw.shao@s2icode.com>
2018-12-18 19:17:30 +0800

    托运单list展示

--- src/app/admin/consignment-notes/consignment-notes.service.ts
+++ src/app/admin/consignment-notes/consignment-notes.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
-import {SharedService} from "../../shared/shared.service";
-import {Observable} from "rxjs/index";
-import {HttpClient} from "@angular/common/http";
+import {SharedService} from '../../shared/shared.service';
+import {Observable} from 'rxjs/index';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
@@ -10,6 +10,45 @@ export class ConsignmentNotesService {
 
     constructor(private http: HttpClient) {
     }
+
+    /**
+     * 获取用户信息集合 需要登录
+     * @returns {Observable<Object>}
+     */
+    getConsignmentNotes(): Observable<any> {
+        return this.http.get(SharedService.getUrl + '/consignmentnote', SharedService.getLoginHttpHeader);
+    }
+
+
+    /**
+     * 通过id查询详细信息
+     * @param {number} id
+     * @returns {Observable<Object>}
+     */
+    getConsignmentNoteById(id): Observable<Object> {
+        return this.http.get(SharedService.getUrl + '/consignmentnote/' + id, SharedService.getLoginHttpHeader);
+    }
+
+
+    /**
+     * 添加托运单
+     * @param {Object} jsonData
+     * @returns {Observable<Object>}
+     */
+    postConsignmentNotes(jsonData: Object): Observable<Object> {
+        return this.http.post(SharedService.getUrl + '/consignmentnote', jsonData, SharedService.getLoginHttpHeader);
+    }
+
+
+    /**
+     *put修改
+     * @param {Object} jsonData
+     * @returns {Observable<Object>}
+     */
+    putConsignmentNotes(id, jsonData: Object): Observable<Object> {
+        return this.http.put(SharedService.getUrl + '/consignmentnote/', jsonData, SharedService.getLoginHttpHeader);
+    }
+
 }
 
 
@@ -69,45 +108,5 @@ export class ConsignmentNote {
     // private Use  user;
     // private ConsignmentNoteStatus consignmentNoteStatus;
     // private Vehicle vehicle;
-    /**
-     * 获取用户信息集合 需要登录
-     * @returns {Observable<Object>}
-     */
-    getConsignmentNotes(): Observable<any> {
-        return this.http.get(SharedService.getUrl + '/consignmentnote', SharedService.getLoginHttpHeader);
-    }
-
-
-    /**
-     * 通过id查询详细信息
-     * @param {number} id
-     * @returns {Observable<Object>}
-     */
-    getConsignmentNoteById(id): Observable<Object> {
-        return this.http.get(SharedService.getUrl + '/consignmentnote/' + id, SharedService.getLoginHttpHeader);
-    }
-
-
-    /**
-     * 添加托运单
-     * @param {Object} jsonData
-     * @returns {Observable<Object>}
-     */
-    postConsignmentNotes(jsonData: Object): Observable<Object> {
-        return this.http.post(SharedService.getUrl + '/consignmentnote', jsonData, SharedService.getLoginHttpHeader);
-    }
-
-
-    /**
-     *put修改
-     * @param {Object} jsonData
-     * @returns {Observable<Object>}
-     */
-    putConsignmentNotes(id, jsonData: Object): Observable<Object> {
-        return this.http.put(SharedService.getUrl + '/consignmentnote/', jsonData, SharedService.getLoginHttpHeader);
-    }
 
 }
-
-
-
