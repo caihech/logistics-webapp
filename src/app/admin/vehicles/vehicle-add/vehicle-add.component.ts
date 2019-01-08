4d7fb9306ebfc41a8a468e2072818119e7eb49d6
Dawei Shao <dw.shao@s2icode.com>
2019-01-08 14:09:43 +0800

    添加马单流畅 图完成

--- src/app/admin/vehicles/vehicle-add/vehicle-add.component.ts
+++ src/app/admin/vehicles/vehicle-add/vehicle-add.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
 import {SelectionModel} from '@angular/cdk/collections';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -9,10 +10,33 @@ import {SelectionModel} from '@angular/cdk/collections';
     styleUrls: ['./vehicle-add.component.css']
 })
 export class VehicleAddComponent implements OnInit {
-    displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
+    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'star'];
     dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-    selection = new SelectionModel<PeriodicElement>(true, []);
+    selection = new SelectionModel<PeriodicElement2>(true, []);
+
+    displayedColumns2: string[] = ['select', 'position', 'name', 'weight', 'symbol', 'goodsName', 'weight1'];
+    dataSource2 = new MatTableDataSource<PeriodicElement2>(ELEMENT_DATA);
+    selection2 = new SelectionModel<PeriodicElement2>(true, []);
+
+    isLinear = false;
+    firstFormGroup: FormGroup;
+    secondFormGroup: FormGroup;
     @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatPaginator) paginator2: MatPaginator;
+
+    constructor(private _formBuilder: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource2.paginator = this.paginator2;
+        this.firstFormGroup = this._formBuilder.group({
+            firstCtrl: ['', Validators.required]
+        });
+        this.secondFormGroup = this._formBuilder.group({
+            secondCtrl: ['', Validators.required]
+        });
+    }
 
     isAllSelected() {
         const numSelected = this.selection.selected.length;
@@ -27,9 +51,6 @@ export class VehicleAddComponent implements OnInit {
             this.dataSource.data.forEach(row => this.selection.select(row));
     }
 
-    ngOnInit() {
-        this.dataSource.paginator = this.paginator;
-    }
 
 }
 
@@ -40,7 +61,38 @@ export interface PeriodicElement {
     symbol: string;
 }
 
+export interface PeriodicElement2 {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+}
+
 const ELEMENT_DATA: PeriodicElement[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+    {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+    {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+    {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+    {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+    {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+    {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+    {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+    {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+    {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+    {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+];
+
+
+const ELEMENT_DATA2: PeriodicElement[] = [
     {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
     {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
     {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
