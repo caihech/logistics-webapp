80fe0e293922849b9a44763599756f1c7d342e4f
Cai He <h.cai@s2icode.com>
2019-02-16 16:31:40 +0800

    编写form表单元素

--- src/app/admin/consignment-notes/consignment-note-add/consignment-note-add.component.ts
+++ src/app/admin/consignment-notes/consignment-note-add/consignment-note-add.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
     selector: 'app-consignment-note-add',
@@ -7,12 +8,100 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ConsignmentNoteAddComponent implements OnInit {
 
-    email = '';
+    ordersFormModel: FormGroup;
 
-    constructor() {
+    constructor(fb: FormBuilder) {
+
+        this.ordersFormModel = fb.group({
+            // 订单编号 8位 唯一 必填
+            orderNumber: [],
+            // 站点 必填 40字节
+            station: [],
+            // 货号 必填 20 字节
+            articleNumber: [],
+            // 托运日期 必填 日期
+            consignmentDate: [],
+            // 托运人 姓名 必填 10字节
+            shippersName: [],
+            // 托运人 电话 必填 15字节
+            shippersPhone: [],
+            // 收货人 姓名 必填 10字节
+            consigneeName: [],
+            // 收货人 电话 必填 15字节
+            consigneePhone: [],
+
+            // 货物名称 必填 80 字节
+            goodsName: [],
+            // 包装 100
+            packaging: [],
+            // 重量 必填 0.000
+            weight: [],
+            // 体积 必填 0.000
+            volume: [],
+            // 件数 必填 int
+            number: [],
+            // 保险 必填  0.00
+            insurance: [],
+            // 保费= 保险*0.005   不可编辑
+            premium: [],
+            // 月结 不可编辑 公式计算：
+            monthlyStatement: [],
+            // 回单付 0.00
+            receiptPayment: [],
+            // 现金付 0.00
+            cashPayment: [],
+            // 提付 必填 0.00
+            extractPayment: [],
+            // 短途运费
+            shortHaulFreight: [],
+            // 代收货款 0.00
+            collectionOnDelivery: [],
+            // 合计 不可编辑 计算公式:保费+月结+回单付+现金+提付+短途运费+代收货款
+            amount: [],
+            // 送货地址 80 字节
+            deliveryAddress: [],
+            // remark
+            remark: [],
+            // 打印次数
+            printCount: []
+
+
+        });
+
     }
 
     ngOnInit() {
+    }
+
+
+    /**
+     * 提交表单
+     */
+    onSubmitForm() {
+        var _that = this;
+        if (this.ordersFormModel.valid) {
+            // _that.userService.postUser(this.userFormModel.value).subscribe((res) => {
+            //     alert('添加成功');
+            //     _that.router.navigate(['/admin/users']);
+            // }, (error) => {
+            //     if (error['status'] === 400) {
+            //         alert('数据格式错误');
+            //     } else if (error['status'] === 401) {
+            //         alert('Token已过期');
+            //     } else if (error['status'] === 500) {
+            //         alert('用户名已存在');
+            //     } else {
+            //         alert('其他错误状态码' + error['status']);
+            //     }
+            // });
+        }
+    }
+
+
+    /**
+     * 清空表单
+     */
+    onResetForm() {
     }
 
 
