e62dd048ea0ee823e98cf89730710d534d80365f
Cai He <h.cai@s2icode.com>
2019-02-24 23:21:22 +0800

    委托单集合页面

--- src/app/admin/consignment-notes/consignment-notes.component.ts
+++ src/app/admin/consignment-notes/consignment-notes.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {ConsignmentNotesService} from './consignment-notes.service';
-import {RolesService} from '../roles/roles.service';
 
 
 @Component({
@@ -14,7 +13,7 @@ export class ConsignmentNotesComponent implements OnInit {
 
     consignmentTableDisplayedColumns: string[] = ['id', 'orderNumber', 'articleNumber', 'consignmentDate', 'shippersName', 'shippersPhone',
         'goodsName', 'weight', 'number', 'delete', 'edit'];
-    // consignmentTableDataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+    consignmentTableDataSource;
     @ViewChild(MatPaginator) paginator: MatPaginator;
     @ViewChild(MatSort) sort: MatSort;
 
@@ -22,14 +21,9 @@ export class ConsignmentNotesComponent implements OnInit {
     constructor(private consignmentNotesService: ConsignmentNotesService) {
     }
 
+
     ngOnInit() {
         this.httpGetConsignmentNotes();
-        // console.info('aaa');
-        // this.roleservice.getRoles().subscribe((res) => {
-        //     console.info(res);
-        // }, (error) => {
-        //     console.info(error);
-        // });
     }
 
 
@@ -37,25 +31,17 @@ export class ConsignmentNotesComponent implements OnInit {
      * 获取托运单信息
      */
     httpGetConsignmentNotes() {
-        const _that = this;
+        var _that = this;
         _that.consignmentNotesService.getConsignmentNotes().subscribe(
             function (res) {
                 console.info(res);
-                // _that.consignmentTableDataSource = new MatTableDataSource<Object>(res);
-                // _that.consignmentTableDataSource.paginator = _that.paginator;
-                // _that.consignmentTableDataSource.sort = _that.sort;
+                _that.consignmentTableDataSource = new MatTableDataSource<Object>(res);
+                _that.consignmentTableDataSource.paginator = _that.paginator;
+                _that.consignmentTableDataSource.sort = _that.sort;
             }, function (error) {
                 console.error(error);
             }
         );
-    }
-
-    /**
-     * 编辑托运单id
-     * @param {number} id
-     */
-    onEdit(id: number) {
-        alert('编辑托运单' + id);
     }
 
     /**
@@ -66,17 +52,4 @@ export class ConsignmentNotesComponent implements OnInit {
         alert('删除托运单' + id);
     }
 
-}
-
-
-export interface PeriodicElement {
-    id: number;
-    orderNumber: string;
-    articleNumber: string;
-    consignmentDate: string;
-    shippersName: string;
-    shippersPhone: string;
-    goodsName: string;
-    weight: string;
-    number: string;
 }
