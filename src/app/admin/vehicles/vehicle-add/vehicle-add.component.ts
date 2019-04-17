0b480eee58ea52afb4a1963d884851ec314593e2
Cai He <h.cai@s2icode.com>
2019-04-17 14:16:01 +0800

    车次添加页面功能

--- src/app/admin/vehicles/vehicle-add/vehicle-add.component.ts
+++ src/app/admin/vehicles/vehicle-add/vehicle-add.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {VehiclesService} from '../vehicles.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-vehicle-add',
@@ -9,11 +11,50 @@ import {FormBuilder} from '@angular/forms';
 export class VehicleAddComponent implements OnInit {
 
 
-    constructor(private _formBuilder: FormBuilder) {
+    vehicleFormModel: FormGroup;
+
+    constructor(private fb: FormBuilder, private vehiclesService: VehiclesService, private router: Router) {
+
+        this.vehicleFormModel = fb.group({
+            licensePlate: [],
+            driverName: [],
+            drivingLicenseNumber: [],
+            driverPhone: [],
+            startDate: [],
+            endDate: []
+        });
     }
 
     ngOnInit() {
 
+    }
+
+
+    /**
+     * 提交表单
+     */
+    onSubmitForm() {
+        var _that = this;
+        if (this.vehicleFormModel.valid) {
+            _that.vehiclesService.postVehicles(_that.vehicleFormModel.value).subscribe(
+                (res) => {
+                    alert('添加成功');
+                    _that.router.navigate(['/admin/vehicles']);
+                },
+                (error) => {
+                    if (error['status'] === 400) {
+                        alert('数据格式错误');
+                    } else if (error['status'] === 401) {
+                        alert('Token已过期');
+                    } else if (error['status'] === 403) {
+                        alert('您权限不够');
+                    } else if (error['status'] === 500) {
+                        alert('服务器异常');
+                    } else {
+                        alert('其他错误状态码' + error['status']);
+                    }
+                });
+        }
     }
 
 }
