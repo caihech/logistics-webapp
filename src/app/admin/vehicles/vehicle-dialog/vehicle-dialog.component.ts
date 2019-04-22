9b363adeccfb5e6d5826ad152a89c3d950f86567
Cai He <h.cai@s2icode.com>
2019-04-22 22:45:05 +0800

    todo  修改调整后台批量添加托运单

--- src/app/admin/vehicles/vehicle-dialog/vehicle-dialog.component.ts
+++ src/app/admin/vehicles/vehicle-dialog/vehicle-dialog.component.ts
@@ -1,24 +1,67 @@
-import { Component,Inject, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {SharedService} from '../../../shared/shared.service';
 import {ConsignmentNotesService} from '../../../service/consignment-notes.service';
 
 
 @Component({
-  selector: 'app-vehicle-dialog',
-  templateUrl: './vehicle-dialog.component.html',
-  styleUrls: ['./vehicle-dialog.component.css']
+    selector: 'app-vehicle-dialog',
+    templateUrl: './vehicle-dialog.component.html',
+    styleUrls: ['./vehicle-dialog.component.css']
 })
 export class VehicleDialogComponent implements OnInit {
 
-  constructor( @Inject(MAT_DIALOG_DATA)private data: VehicleDialogData,private consignmentNotesService: ConsignmentNotesService) { }
+    consignmentNotesList: [] = [];
 
-  ngOnInit() {
-  }
+    constructor(@Inject(MAT_DIALOG_DATA) private data: VehicleDialogData
+        , private consignmentNotesService: ConsignmentNotesService) {
+    }
 
+    ngOnInit() {
+        this.initWebData();
+    }
+
+
+    initWebData() {
+        var _that = this;
+        _that.consignmentNotesService.getConsignmentNotes().subscribe(
+            (res) => {
+                for (let temp of res) {
+
+                    if (temp['valid'] === true && temp['vehicleId'] === 0) {
+                        // console.info(temp);
+                        let cns = {
+                            id: temp['id'],
+                            orderNumber: temp['orderNumber'],
+                            selected: false
+                        }
+                        _that.consignmentNotesList.push(cns);
+                    }
+                }
+            },
+            (error) => {
+                console.error(error);
+            });
+    }
+
+
+    onCheckSubmit() {
+        console.info(this.consignmentNotesList);
+
+
+        var _that = this;
+
+        for (let temp of _that.consignmentNotesList) {
+            if (temp.selected === true) {
+                console.info(temp.id);
+            }
+        }
+
+        //TODO 修改啊后台实现传入数组
+    }
 }
 
 
 export interface VehicleDialogData {
-  id: number;
+    id: number;
 }
